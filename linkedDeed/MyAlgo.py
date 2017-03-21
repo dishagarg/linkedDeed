52c35e23c6c583e77c7ada889ee748137fda3539
Disha Garg <garg.disha8@gmail.com>
2017-03-20 20:16:57 -0700

    cleanup

--- linkedDeed/MyAlgo.py
+++ linkedDeed/MyAlgo.py
@@ -11,7 +11,7 @@ filename = 'SkillsetUniversal.txt'
 
 
 def read_txt(file, text):
-    """Reading data from text files."""
+    """Reading data from text files(universal skill list)."""
     with open(file, 'r') as f:
         reader = csv.reader(f)
         for row in reader:
@@ -20,67 +20,78 @@ def read_txt(file, text):
     return text
 
 
-def db_time():
+def select_from():
     """Reading data from tables."""
     conn = sqlite3.connect('stuffToPlot.db')
     c = conn.cursor()
     jobs = []
     for row in c.execute('SELECT DESCRIPTION FROM stuf'):
         jobs.append(row)
-    # print len(jobs)
     c.close()
     c = conn.cursor()
     skills = []
     for row in c.execute('SELECT description FROM stuffToPlot'):
         skills.append(row)
-    # print skills
     c.close()
     conn.close()
     return jobs, skills
 
 
 def jobs_calc(job, text):
-    """Calculate for each job how many skills required with set."""
+    """Calculate how many skills match."""
     temp = 0
     for uni_word in text:
         if uni_word in job:
             temp += 1
     return temp
 
+
+def data_cleanup(jobs, skills, txt):
+    """Clean up of each job description and skills."""
+    for i in range(len(jobs)):
+        jobs[i] = unicodedata.normalize('NFKD', "".join(jobs[i]).replace('\n', ' ').replace('\t', ' ')).encode('ascii','ignore')
+
+    # cz skills will just be in 1 cell
+    skills[0] = unicodedata.normalize('NFKD', "".join(skills[0])).encode('ascii', 'ignore')
+    skill_l = (skills[0].split(', '))
+    skill_list = [item.lower() for item in skill_l]
+    text = [item.lower() for item in txt]
+    jobss = [item.lower() for item in jobs]
+    job_list = []
+    for job in jobss:
+        job_list.append(job.split(' '))
+    return skill_list, text, job_list
+
+
+def accuracy(skill_list, text, job_list):
+    """Calculate match accuracy for each job."""
+    # skills_required = []
+    # skills_match = []
+    match = []  # match depending upon the skills required.
+    for job in job_list:
+        x = jobs_calc(job, text)
+        y = jobs_calc(job, skill_list)
+        # skills_required.append(x)
+        # skills_match.append(y)
+        match.append((float(y) / x) * 100)
+    return match
+
+
+def update_table(match):
+    """Update it in sqlite and order this in desc."""
+    conn = sqlite3.connect('stuffToPlot.db')
+    sql = '''UPDATE stuf SET match = ? WHERE id = ?'''
+    for i in range(len(match)):
+        cur = conn.cursor()
+        cur.execute(sql, match[i], i + 1)
+        conn.commit()
+        cur.close()
+    conn.close()
+
 txt = read_txt(filename, labels)
-jobs, skills = db_time()
-for i in range(len(jobs)):
-    jobs[i] = unicodedata.normalize('NFKD', "".join(jobs[i]).replace('\n', ' ').replace('\t', ' ')).encode('ascii','ignore')
+jobs, skills = select_from()
+skill_list, text, job_list = data_cleanup(jobs, skills, txt)
+skills_you_have = jobs_calc(skill_list, text)
 
-# cz skills will just be in 1 cell
-skills[0] = unicodedata.normalize('NFKD', "".join(skills[0])).encode('ascii', 'ignore')
-skill_l = (skills[0].split(', '))
-skill_list = [item.lower() for item in skill_l]
-text = [item.lower() for item in txt]
-jobss = [item.lower() for item in jobs]
-wtf = []
-for job in jobss:
-    wtf.append(job.split(' '))
-
-what = jobs_calc(skill_list, text)
-what_jobs = []
-worth = []
-match = []
-for job in wtf:
-    x = jobs_calc(job, text)
-    y = jobs_calc(job, skill_list)
-    what_jobs.append(x)
-    worth.append(y)
-    match.append((float(y) / x) * 100)
-
-# update it in sqlite and order this in desc.
-print match
-
-conn = sqlite3.connect('stuffToPlot.db')
-sql = '''UPDATE stuf SET match = ? WHERE id = ?'''
-for i in range(len(match)):
-    cur = conn.cursor()
-    cur.execute(sql, match[i], i + 1)
-    conn.commit()
-    cur.close()
-conn.close()
+match = accuracy(skill_list, text, job_list)
+update_table(match)
